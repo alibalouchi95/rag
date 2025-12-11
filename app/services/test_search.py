3802d09d76730e8ee717d395bcce7743160c56ea
Ali Balouchi <alibalouchi.74@gmail.com>
2025-12-11 17:10:17 +0330

    comment out all the unwanted codes

--- app/services/test_search.py
+++ app/services/test_search.py
@@ -1,13 +1,11 @@
-from database import hf_embeddings, vector_store, COLLECTION_NAME, qdrant_client_raw
+# # Count entities
+# count = qdrant_client_raw.count(COLLECTION_NAME).count
+# print(f"Entities: {count}")
 
-# Count entities
-count = qdrant_client_raw.count(COLLECTION_NAME).count
-print(f"Entities: {count}")
+# # --- Embed query ---
+# query = "what is SDN?"
+# embedded_query = hf_embeddings.embed_query(query)
+# print(f"Embedding length: {len(embedded_query)}")
 
-# --- Embed query ---
-query = "what is SDN?"
-embedded_query = hf_embeddings.embed_query(query)
-print(f"Embedding length: {len(embedded_query)}")
-
-# --- Run search using LangChain wrapper ---
-print("\nRunning similarity search...")
+# # --- Run search using LangChain wrapper ---
+# print("\nRunning similarity search...")
